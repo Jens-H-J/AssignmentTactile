f58863477801412e591753c64052b745b363eb9f
Jens Jakobsen <jejak14@student.sdu.dk>
2022-09-06 15:33:29 +0200

    Assingment 1
    Created with for loops and some  if statements making sure the last itteration of the for-loops did not count into the calculations. Test are at the top

--- TactileAssignemt/TactileAssignemt/Program.cs
+++ TactileAssignemt/TactileAssignemt/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+
+class AssignmentOne
+{
+    
+
+    static int NaturalMaks(int Maks) { 
+
+    
+        int InitialNr = 0;
+        int Total = 0;
+
+        for(int i = 0; InitialNr < Maks; i++)
+        {
+            InitialNr = i * 3;
+
+            if(InitialNr < Maks)
+            {
+                Total = Total + InitialNr;
+            }
+            
+        }
+
+        InitialNr = 0;
+
+        for (int i = 0; InitialNr < Maks; i++)
+        {
+            InitialNr = i * 5;
+
+            if (InitialNr < Maks)
+            {
+                Total = Total + InitialNr;
+            }
+
+        }
+
+
+        return Total;
+    }
+
+    public static void Main()
+    {
+
+        Console.Write("Enter an Maks(typed nr not inkl.): ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write(NaturalMaks(n));
+    }
+
+
+}
