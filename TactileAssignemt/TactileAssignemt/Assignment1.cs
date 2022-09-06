b807dfdde996e4119689742618dbd7750dfd3121
Jens Jakobsen <jejak14@student.sdu.dk>
2022-09-06 16:43:19 +0200

    Rewrote assignment 2 + task complete
    While i know im above time i rewrote the code to make it work

--- TactileAssignemt/TactileAssignemt/Assignment1.cs
+++ TactileAssignemt/TactileAssignemt/Assignment1.cs
@@ -16,11 +16,10 @@ class AssignmentOneTwo
         int n = int.Parse(Console.ReadLine());
         */
 
-        /*int n = 1000;
-        Console.Write(NaturalMaks(n));*/
+        int n = 1000;
+        int m = 3;
+        Console.Write("assingment 1 " + NaturalMaks(n) + " assignment 2 " + Palindrome(m));
 
-        int m = 2;
-        Console.Write(Palindrome(m));
 
     }
 
@@ -66,79 +65,33 @@ class AssignmentOneTwo
         {
         int upper_limit = (int)Math.Pow(10, n) - 1;
 
-
-        // largest number of n-1 digit.
-        // One plus this number
-        // is lower limit which is
-        // product of two numbers.
         int lower_limit = 1 + upper_limit / 10;
 
-        // Initialize result
         int max_product = 0;
 
         for (int i = upper_limit; i >= lower_limit; i--)
         {
             for (int j = i; j >= lower_limit; j--)
             {
-                // calculating product of two
-                // n-digit numbers
+
                 int product = i * j;
                 if (product < max_product)
                     break;
                 int number = product;
                 int reverse = 0;
 
-                // calculating reverse of product
-                // to check whether it is
-                // palindrome or not
                 while (number != 0)
                 {
                     reverse = reverse * 10 + number % 10;
                     number /= 10;
                 }
 
-                // update new product if exist and if
-                // greater than previous one
                 if (product == reverse && product > max_product)
                     max_product = product;
             }
         }
         return max_product;
+
     }
-    /*
-        int Maks = (int)Math.Pow(10, number) - 1;
-        int Min = 1 + Maks / 10;
 
-        int result = 0;
-
-        for (int i = Maks; i >= Min; i--)
-        {
-            for (int j = i; j >= Min; j--)
-            {
-                //calculations
-                int product = i * j;
-                if (product < result)
-                    break;
-                int ProductNumber = product;
-                int Check = 0;
-
-                // checking if number is a palindrome
-
-                while (ProductNumber != 0)
-                {
-                    Check = Check * 10 + ProductNumber % 10;
-                    ProductNumber /= 10;
-                }
-
-                if (product == ProductNumber && product > result)
-                {
-                    result = product;
-                }
-            }
-        }
-
-        return result;*/
-
-}
-
-    
+    }
